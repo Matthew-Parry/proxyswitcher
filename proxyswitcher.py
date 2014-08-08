eb0b06cccaa635860fb1719587e797978f199ec0
Matthew Parry <matthew@becsalana.co.uk>
2014-08-08 10:28:03 +0100

    Initial docs

---
+++ proxyswitcher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#
+# proxyswitcher.py
+# 
+# Save proxy settings
+# Set/unset proxy settings when selected
+#
+# Matthew Parry 2014
+#
+
+# -----------------------
+# Import required Python libraries
+# -----------------------
+import time
+import os
+
+# GUI setup
+
+# get proxy settings from GUI
+
+# save proxy settings in file
+# parse string
+# save as file
+
+# edit proxy settings
+# call get
+# call save
+
+# get proxy settings from file
+# find file
+# read file into variables
+
+# delete proxy settings
+# call get
+# delete it if sure
+
+# set proxy settings
+# call get
+# parse variables
+# export environment variables
+# create aptconf file using parsed variables
+# save/overwrite aptconf file
+
+# unset proxy settings
+# unset environment variables
+# create blank aptconf file
+# save/overwrite aptconf file
